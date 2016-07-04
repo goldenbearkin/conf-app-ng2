be7ac0521b2833bcdb515e596a66131c4a7d011a
jasonhui <huihangkin@gmail.com>
2016-07-05 00:14:35 +0800

    filters (two-way binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import { FiltersComponent } from './filters/filters.component'
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [FiltersComponent]
 })
 export class AppComponent {
-  title = 'app works!';
 }
---
+++ src/app/filters/filters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-filters',
+  templateUrl: 'filters.component.html',
+  styleUrls: ['filters.component.css']
+})
+export class FiltersComponent {
+  speaker: String;
+}
